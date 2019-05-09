33f13ae54cf93c728fa91b508d1ef192de05162b
Liluyoud Cury de Lacerda <liluyoud@hotmail.com>
2019-05-09 12:27:49 -0400

    Adicionar arquivos de projeto.

---
+++ BazarSapiens/Helpers/HtmlHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BazarSapiens.Helpers
+{
+    public class PaginaHelper
+    {
+        public static string PaginaAtual(ViewContext context, string pagina, string classe)
+        {
+            string paginaAtual = context.RouteData.Values["Page"].ToString();
+            return (paginaAtual.ToLower().IndexOf(pagina) >= 0) ? classe : "";
+        }
+    }
+}
---
+++ BazarSapiens/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BazarSapiens
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
